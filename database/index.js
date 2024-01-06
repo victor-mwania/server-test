6b5cda6e37326dca37fe7300da6205237342c878
Victor Mwania <victormwnza@gmail.com>
2024-01-06 18:37:41 +0300

    Remove mongodb credentials

--- database/index.js
+++ database/index.js
@@ -3,21 +3,6 @@ const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 
 
-// module.exports = async () => {
-//     try {
-//       const db = await mongoose.connect('mongodb+srv://victormwnza:nrwBVt1gtoreXvoe@cluster0.hreqbpm.mongodb.net/?retryWrites=true&w=majority' , { useNewUrlParser: true, useUnifiedTopology: true });
-//       console.log(
-//         `Connected to Mongo! Database name: "${db.connections[0].name}"`
-//       );
-//       return db;
-//     } catch (err) {
-//       console.error(`Error connecting to mongo: ${err}`);
-//       return err;
-//     }
-//   };
-
-
-
 exports.dbConnect = async () => {
     const mongo = await MongoMemoryServer.create();
     const uri = mongo.getUri();
